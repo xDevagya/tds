4cb29943ef64a9db12eee9d7a1423b37c866eb6c
Devagya Awasthi <47398849+xDevagya@users.noreply.github.com>
2024-04-21 17:28:48 +0530

    Update main.py
    Finishing up

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-n1 = float(input("Enter first number: "))
-n2 = float(input("Enter second number: "))
-n3 = float(input("Enter third number: "))
+import streamlit as st
 
-print(max(n1,n2,n3))
+st.set_page_config(page_title="Largest Number App", page_icon=":bar_chart:")
+
+st.header("Largest Number App")
+st.write("A simple app to find the largest of three numbers. Made by 22f3001390(Devagya)")
+
+with st.form(key="input_form"):
+    n1 = st.number_input("Enter first number:")
+    n2 = st.number_input("Enter second number:")
+    n3 = st.number_input("Enter third number:")
+    calc_button = st.form_submit_button("Find Largest Number")
+
+if calc_button:
+    largest_num = max(n1, n2, n3)
+    st.success(f"The largest number is: {largest_num}")
