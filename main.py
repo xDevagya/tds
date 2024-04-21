d09610f1c3bb97f68349c5d9f2b8442888129984
Devagya Awasthi <47398849+xDevagya@users.noreply.github.com>
2024-04-21 17:39:20 +0530

    step +/- int

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ st.header("Largest Number App")
 st.write("A simple app to find the largest of three numbers. Made by 22f3001390(Devagya)")
 
 with st.form(key="input_form"):
-    n1 = st.number_input("Enter first number:")
-    n2 = st.number_input("Enter second number:")
-    n3 = st.number_input("Enter third number:")
+    n1 = st.number_input("Enter first number:", step=1)
+    n2 = st.number_input("Enter second number:", step=1)
+    n3 = st.number_input("Enter third number:", step=1)
     calc_button = st.form_submit_button("Find Largest Number")
 
 if calc_button:
